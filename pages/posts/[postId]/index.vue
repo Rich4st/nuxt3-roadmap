9416a97c107a269b5d1bf704438367366d0402cb
Kooboo <valk@kooboo.cn>
2022-11-01 14:16:00 +0800

    first commit

---
+++ pages/posts/[postId]/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+const route = useRoute()
+const { postId } = route.params
+</script>
+
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-3xl text-gray-600">
+        這是 posts/[postId] 的頁面
+      </h1>
+      <p class="my-8 text-3xl text-gray-600">
+        匹配到的 Post Id: <span class="text-5xl font-semibold text-blue-600">{{ postId }}</span>
+      </p>
+    </div>
+  </div>
+</template>
