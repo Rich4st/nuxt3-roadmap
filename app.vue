d4fb226e60f01562673040f67f5e1e8f76701fe7
Kooboo <valk@kooboo.cn>
2022-11-01 14:47:58 +0800

    doc: default & custom layouts

--- app.vue
+++ app.vue
@@ -2,7 +2,12 @@
 </script>
 
 <template>
-  <div>
-    <NuxtPage />
+  <div class="m-4 bg-white">
+    <p class="pb-4 text-2xl text-slate-600">
+      這裡是最外層 app.vue
+    </p>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
