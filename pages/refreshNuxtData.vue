103a572b69bfd888b61da776714e616049177c8e
Rich4 <zmingcheng204@gmail.com>
2022-11-05 15:22:09 +0800

    doc: data fetch

---
+++ pages/refreshNuxtData.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { pending, data: count } = useLazyAsyncData('count', () => $fetch('/api/count'))
+
+const refresh = () => refreshNuxtData('count')
+</script>
+
+<template>
+  <div>
+    {{ pending ? 'Loading' : count }}
+  </div>
+  <button @click="refresh">
+    Refresh
+  </button>
+</template>
+
---
+++ pages/useAsyncData.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// const { data } = useAsyncData('count', () => $fetch('/api/count'))
+const { data } = useLazyAsyncData('count', () => $fetch('/api/count'), {
+  default: () => '-',
+})
+</script>
+
+<template>
+  <div class="my-24 flex flex-col items-center">
+    <p class="text-4xl text-gray-600">
+      瀏覽次數
+    </p>
+    <span class="mt-4 text-6xl font-semibold text-sky-400">{{ data }}</span>
+  </div>
+</template>
